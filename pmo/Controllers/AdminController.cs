7fbd871094bfa56f376073a02e5b7d234a448944
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-01-14 12:29:33 +0200

    add admin controller

---
+++ pmo/Controllers/AdminController.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using ViewModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace pmo.Controllers {
+
+       [Route("admin")]
+       public class AdminController : BaseController {
+        public AdminController(EfContext context, IMapper mapper) : base(context, mapper) {
+            
+        }
+
+        [Route("")]
+        public IActionResult Index(){
+
+            var stageConfig = _context.StageConfigs.Include(x=>x.RequiredDesignConcepts).Include(x=>x.RequiredSchedules).ToList();
+            var stageConfigViewModel = _mapper.Map<List<StageConfigViewModel>>(stageConfig);
+
+            var gateConfig = _context.GateConfigs.ToList();
+            var gateConfigViewModel = _mapper.Map<List<GateConfigViewModel>>(gateConfig);
+
+            var roles = _context.Roles.ToList();
+            var roleViewModel = _mapper.Map<List<RoleViewModel>>(roles);
+
+            var tags = _context.Tags.Include(x=>x.TagCategory).ToList();
+            var tagViewModel = _mapper.Map<List<TagViewModel>>(tags);
+
+            AdminViewModel vm = new AdminViewModel { 
+                StageConfigViewModel = stageConfigViewModel, 
+                GateConfigViewModel = gateConfigViewModel, 
+                RolesViewModel= roleViewModel, 
+                TagViewModel= tagViewModel
+            };       
+
+            return View(vm);
+        }
+
+        
+    }
+}
