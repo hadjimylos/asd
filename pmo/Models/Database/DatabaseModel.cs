851d01e9136af77c49d8a9fd38f5d50e31fcd8ec
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-01-14 11:04:06 +0200

    alter primary key to not nullable type

--- pmo/Models/Database/DatabaseModel.cs
+++ pmo/Models/Database/DatabaseModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace dbModels {
     public class DatabaseModel {
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int? Id { get; set; }
+        public int Id { get; set; }
         
         public DateTime CreateDate { get; set; }
         
