ec7aab869ebe555ddf7a5544b0fddf0e2415cbc1
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-01-14 11:05:07 +0200

    add base controller

---
+++ pmo/Controllers/BaseController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+
+namespace pmo.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected readonly EfContext _context;
+        protected readonly IMapper _mapper;
+        public BaseController(EfContext context,  IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public BaseController()
+        {
+        }
+    }
+}
