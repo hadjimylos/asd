7bd24ee754c42fe4ea91a861f44a9b9cc71e6822
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-01-22 13:59:34 +0200

    cleanup

--- pmo/Controllers/Admin/AdminController.cs
+++ pmo/Controllers/Admin/AdminController.cs
@@ -15,31 +15,7 @@ namespace pmo.Controllers {
 
         [Route("")]
         public IActionResult Index(){
-
-            var stageConfig = _context.StageConfigs.Include(x=>x.RequiredDesignConcepts).Include(x=>x.RequiredSchedules).ToList();
-            var stageConfigViewModel = _mapper.Map<List<StageConfigViewModel>>(stageConfig);
-
-            var gateConfig = _context.GateConfigs.ToList();
-            var gateConfigViewModel = _mapper.Map<List<GateConfigViewModel>>(gateConfig);
-
-            var roles = _context.Roles.ToList();
-            var roleViewModel = _mapper.Map<List<RoleViewModel>>(roles);
-
-            var tags = _context.Tags.Include(x=>x.TagCategory).ToList();
-            var tagViewModel = _mapper.Map<List<TagViewModel>>(tags);
-
-            var users = _context.Users.Include(x => x.Citizenships).ToList();
-            var userViewModel = _mapper.Map<List<UserViewModel>>(users);
-
-            AdminViewModel vm = new AdminViewModel { 
-                StageConfigViewModel = stageConfigViewModel, 
-                GateConfigViewModel = gateConfigViewModel, 
-                RolesViewModel= roleViewModel, 
-                TagViewModel= tagViewModel,
-                UserViewModel = userViewModel
-            };       
-
-            return View(vm);
+            return View();
         }
 
         
