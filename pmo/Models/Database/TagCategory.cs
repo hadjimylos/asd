0f0d66623dd43f9c159623f2960f74be1bfd1501
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-01-23 16:38:13 +0200

    add reusable method to recieve dropdowns from tagcategory

--- pmo/Models/Database/TagCategory.cs
+++ pmo/Models/Database/TagCategory.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace dbModels {
     public class TagCategory: DatabaseModel {
@@ -11,5 +13,11 @@ namespace dbModels {
         //[ForeignKey("TagId")]
         public virtual List<Tag> Tags { get;set; }
 
+        public List<SelectListItem> GetListItems(List<int> selected) =>
+            this.Tags.Select(s => new SelectListItem {
+                    Text = s.Name,
+                    Value = s.Id.ToString(),
+                    Selected = selected.Contains(s.Id),
+                }).ToList();
     }
 }
