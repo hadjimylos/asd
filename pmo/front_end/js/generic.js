1f2207796e7c54a38f507f302acd1712844048ec
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-01-27 15:59:20 +0200

    add nav component

--- pmo/front_end/js/generic.js
+++ pmo/front_end/js/generic.js
@@ -15,3 +15,20 @@ $('.pane').on('click', function(e) {
 	var target = $(e.currentTarget).data('panel');
 	$('#' + target).toggleClass('hide');
 });
+
+// set height of nav component to screen on load
+const navGroups = document.querySelector('.nav-component .wrapper .side-nav-groups');
+if (navGroups) {
+	const fixedHeight = document.body.scrollHeight > window.innerHeight ? document.body.scrollHeight : window.innerHeight;
+	navGroups.style.height = `${fixedHeight}px`;
+
+	const anchors = navGroups.querySelectorAll('a');
+	if (anchors.length > 0) {
+		var evenHeight = (window.innerHeight) / anchors.length;
+		anchors.forEach((anchor) => {
+			anchor.style.marginBottom = `${evenHeight - 15}px`;
+		});
+	}
+
+	
+}
