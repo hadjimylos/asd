c85e7a0cad7f9950e03f6b8760efde1f3155da78
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-01-28 11:23:04 +0200

    add directory services dependency for user validation

--- pmo/Models/Helpers/ContextHelpers.cs
+++ pmo/Models/Helpers/ContextHelpers.cs
@@ -1,4 +1,26 @@
-﻿namespace ViewModels.Helpers {
+﻿using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+
+namespace ViewModels.Helpers {
+    public static class ActiveDirectoryHelper { 
+        public static DirectoryEntry GetUsersManager(string network_username) {
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "GLOBAL")) {
+                using (var foundUser = UserPrincipal.FindByIdentity(pc, IdentityType.SamAccountName, network_username)) {
+                    var user = (DirectoryEntry)foundUser.GetUnderlyingObject();
+                    return user.Properties["manager"].Value == null ? null : new DirectoryEntry("LDAP://" + user.Properties["manager"].Value.ToString());
+                }
+            }
+        }
+
+        public static UserPrincipal GetUser(string network_username) {
+            using (PrincipalContext pc = new PrincipalContext(ContextType.Domain, "GLOBAL")) {
+                using (var foundUser = UserPrincipal.FindByIdentity(pc, IdentityType.SamAccountName, network_username)) {
+                    return foundUser;
+                }
+            }
+        }
+    }
+
     public static class TagCategoryHelper {
         public const string CitizenshipsKey = "Citizenships";
     }
