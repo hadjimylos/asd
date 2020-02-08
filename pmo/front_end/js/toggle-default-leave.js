044d7f45f36083ef9cb519ff9f3e58e4b1a8c081
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-02-08 23:38:44 +0200

    remove front end and separate into separate project, start tracking wwwroot only

--- pmo/front_end/js/toggle-default-leave.js
+++
@@ -1,6 +0,0 @@
-﻿let $ = require('jquery');
-
-$('.toggle-default-isdays').on('change', (e) => {
-    const default_val = e.currentTarget.checked ? 25 : 187.5;
-    document.querySelector('.data.annual-leave').innerText = default_val
-});
