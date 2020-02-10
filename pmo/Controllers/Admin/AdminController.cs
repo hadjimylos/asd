aac82775bdeb971f0085885f6cfb0df47899854b
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-02-10 13:59:15 +0200

    add global admin controller

---
+++ pmo/Controllers/Admin/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace pmo.Controllers {
+    [Route("admin")]
+    public class AdminController : Controller {
+        private readonly string path = "~/Views/Admin/Admin";
+
+        public IActionResult Index() {
+            return View($"{path}/Index.cshtml");
+        }
+    }
+}
