65fb4cc0b9181b6c5cbe41156061e33b50010223
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-02-12 15:16:58 +0200

    rename lookup lists and cleanup

--- pmo/Models/Database/Stage.cs
+++ pmo/Models/Database/Stage.cs
@@ -16,21 +16,18 @@ namespace dbModels
         public virtual Project Project { get; set; }
 
         public int StageNumber { get; set; }
-        public virtual List<Schedule> ScheduleHistory { get; set; }
-        public virtual List<StageDesignConcept> StageDesignConceptHistory { get; set; }
-        public virtual List<Risk> RiskHistory { get; set; }
+        public virtual List<Schedule> Schedules { get; set; }
+        public virtual List<StageDesignConcept> DesignConcepts { get; set; }
+        public virtual List<Risk> Risks { get; set; }
 
         public virtual List<ProjectJustification> ProjectJustificationHistory { get; set; }
         public virtual List<BusinessCase> BusinessCaseHistory { get; set; }
         public virtual List<ProductInfrigmentPatentability> ProductInfrigmentPatentabilityHistory { get; set; }
-
         public virtual List<KeyCharacteristic> KeyCharacteristicHistory { get; set; }
-
         public virtual List<CustomerDesignApproval> CustomerDesignApprovalHistory { get; set; }
         public virtual List<InvestmentPlan> InvestmentPlanHistory { get; set; }
         public virtual List<ProductIntroChecklist> ProductIntroChecklistHistory { get; set; }
         public virtual List<RampResourcePlan> RampResourcePlanHistory { get; set; }
-
         public virtual List<QualificationTesting> QualificationTestingHistory { get; set; }
     }
 }
