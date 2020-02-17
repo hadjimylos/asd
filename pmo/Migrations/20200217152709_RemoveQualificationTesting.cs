31b91f969cdf27126a88d1c3bbfe61a7c532623f
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-02-17 17:27:39 +0200

    remove qualification testing component migration

---
+++ pmo/Migrations/20200217152709_RemoveQualificationTesting.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pmo.Migrations
+{
+    public partial class RemoveQualificationTesting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "QualificationTestings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "QualificationTestings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AttachmentUrl = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsQualificationRequired = table.Column<bool>(type: "bit", nullable: false),
+                    LastModified = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedByUser = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StageId = table.Column<int>(type: "int", nullable: false),
+                    Version = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_QualificationTestings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_QualificationTestings_Stages_StageId",
+                        column: x => x.StageId,
+                        principalTable: "Stages",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_QualificationTestings_StageId",
+                table: "QualificationTestings",
+                column: "StageId");
+        }
+    }
+}
