e3e88355c4a413270a82900c4021389a8acd3faf
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-02-17 16:49:04 +0200

    remove business case file upload field migration

---
+++ pmo/Migrations/20200217144842_RemoveColumnBusinessCase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pmo.Migrations
+{
+    public partial class RemoveColumnBusinessCase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkRequirementUrl",
+                table: "BusinessCases");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WorkRequirementUrl",
+                table: "BusinessCases",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
