02c69184d1f516283ea7013ac12456104943a522
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-03-05 12:12:32 +0200

    alter mitigation fields not nullable

---
+++ pmo/Migrations/20200305101107_MitigationFieldsNotNullable.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace pmo.Migrations
+{
+    public partial class MitigationFieldsNotNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Responsibility",
+                table: "Mitigations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MitigationPlan",
+                table: "Mitigations",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Responsibility",
+                table: "Mitigations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "MitigationPlan",
+                table: "Mitigations",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
