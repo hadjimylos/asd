0f7e296506221c708f07a7a894b7ac5612ec1d03
Efthimios Dellis <efthimios.dellis@itt.com>
2020-03-10 16:14:40 +0200

    add fronten powerpoint

--- pmo/Controllers/VBPD/Application/PowerPointController.cs
+++ pmo/Controllers/VBPD/Application/PowerPointController.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore;
 using pmo.Services.Lists;
 using pmo.Services.PowerPoint;
 using ViewModels.Helpers;
-
+using dto;
 namespace pmo.Controllers.VBPD.Application
 {
     [Route("projects/{projectid}/stages/{stageNumber}/powerpoint")]
@@ -38,8 +38,12 @@ namespace pmo.Controllers.VBPD.Application
             public BusinessCase _bc { get; set; }
             public PostLaunchReview _plr { get; set; }
             public string _gateDescript { get; set; }
+            public decimal npv { get; set; }
+            public decimal roi { get; set; }
+            public decimal paybackperiod { get; set; }
+            public List<FinancialDataforPPT> _financials { get; set; }
 
-            public Project_Data(Project p, Project_User u, List<Schedule> schedules , ProductInfrigmentPatentability pip  ,List<Risk> risk , InvestmentPlan ip , BusinessCase bc ,PostLaunchReview plr ,string gateDescript) {
+            public Project_Data(Project p, Project_User u, List<Schedule> schedules, ProductInfrigmentPatentability pip, List<Risk> risk, InvestmentPlan ip, BusinessCase bc, PostLaunchReview plr, string gateDescript, decimal NPV, decimal ROI, decimal PaybackPeriod, List<FinancialDataforPPT> financials) {
                 _p = p;
                 _u = u;
                 _schedules = schedules;
@@ -49,12 +53,16 @@ namespace pmo.Controllers.VBPD.Application
                 _bc = bc;
                 _plr = plr;
                 _gateDescript = gateDescript;
+                npv = NPV;
+                roi = ROI;
+                paybackperiod = PaybackPeriod;
+                _financials = financials;
             }
         }
 
         [Route("download")]
         public IActionResult Download() {
-   
+
             var p = _context.Projects
                 .Include(i => i.ProjectDetails)
                 .First(w => w.Id == _projectId);
@@ -74,9 +82,33 @@ namespace pmo.Controllers.VBPD.Application
                .Where(x => x.StageId == _stageId && x.Stage.ProjectId == _projectId).ToList();
             InvestmentPlan ip = _context.InvestmentPlans.AsNoTracking().GetLatestVersion(_projectId);
             BusinessCase bc = _context.BusinessCases.IncludeAll().GetLatestVersion(_projectId);
-            PostLaunchReview plr = _context.PostLaunchReviews.AsNoTracking().GetLatestVersion(_projectId);
+            BusinessCase businessCase = new BusinessCase();
+            decimal ROI = bc?.GetROI() ?? 0M;
+            decimal NPV = bc?.GetNPV() ?? 0M;
+            decimal PaybackPeriod = bc?.GetPaybackPeriod() ?? 0M;
+            List<FinancialDataforPPT> financialDatafotPPTs = new List<FinancialDataforPPT>();
+            for (var i = 0; i < bc.FinancialData.Count; i++) {
+                FinancialDataforPPT financials = new FinancialDataforPPT();
+                financials.Year=bc.FinancialData[i].Year;
+                financials.Quantity = bc.FinancialData[i].Quantity;
+                financials.StdCostEstimated = bc.FinancialData[i].StdCostEstimated;
+                financials.SalesPriceEstimated = bc.FinancialData[i].SalesPriceEstimated;
+                financials.CostExtended = bc.FinancialData[i].GetCostExtended();
+                financials.RevenueExtended=bc.FinancialData[i].GetRevenueExtended();
+                financials.StandardMarginPrice= bc.FinancialData[i].GetStandardMarginPrice();
+                financials.StandardMarginPercent= bc.FinancialData[i].GetStandardMarginPercent();
+                financials.GPACapital = bc.FinancialData[i].GPACapital ?? 0M;
+                financials.GPAExpense = bc.FinancialData[i].GPAExpense ?? 0M;
+                financials.QualCosts = bc.FinancialData[i].QualCosts ?? 0M;
+                financials.OtherDevelopmentExpenses = bc.FinancialData[i].OtherDevelopmentExpenses ?? 0M;
+                financials.TotalExpenses=bc.FinancialData[i].GetTotalExpenses();
+                financialDatafotPPTs.Add(financials);
+            }
+            
+
+           PostLaunchReview plr = _context.PostLaunchReviews.AsNoTracking().GetLatestVersion(_projectId);
             string GateDescription = _stageNumber == 5 ? "PLR" : _stageNumber.ToString();
-            Project_Data project_data = new Project_Data(p, u, schedules, pip, risk, ip, bc, plr, GateDescription);
+            Project_Data project_data = new Project_Data(p, u, schedules, pip, risk, ip, bc, plr, GateDescription, ROI, NPV, PaybackPeriod, financialDatafotPPTs);
             var dataObj = JsonConvert.SerializeObject(project_data, Formatting.Indented, new JsonSerializerSettings { ReferenceLoopHandling=ReferenceLoopHandling.Ignore});
             return new JsonResult(dataObj);
             
---
+++ pmo/Models/ViewModel/DTO/FinancialDataforPPT.cs
@@ -0,0 +1,30 @@
+﻿namespace dto {
+    public class FinancialDataforPPT {
+        public  int Year { get; set; }
+
+        public  int Quantity { get; set; }
+
+        public  decimal StdCostEstimated { get; set; }
+
+        public  decimal SalesPriceEstimated { get; set; }
+
+        public decimal CostExtended { get; set; }
+          
+
+        public decimal RevenueExtended { get; set; }
+         
+
+        public decimal StandardMarginPrice { get; set; }
+          
+
+        public decimal StandardMarginPercent { get; set; }
+
+        public  decimal GPACapital { get; set; }
+        public  decimal GPAExpense { get; set; }
+        public  decimal QualCosts { get; set; }
+        public  decimal OtherDevelopmentExpenses { get; set; }
+
+        public decimal TotalExpenses { get; set; }
+
+    }
+}
