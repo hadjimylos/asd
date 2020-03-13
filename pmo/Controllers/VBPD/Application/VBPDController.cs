c7fa102c857461d2281028a02f0c5c0eb294695e
Chris Hadjimylos <christoph.hadjimylo@itt.com>
2020-03-13 13:32:38 +0200

    add status and state to home list

--- pmo/Controllers/VBPD/Application/VBPDController.cs
+++ pmo/Controllers/VBPD/Application/VBPDController.cs
@@ -94,7 +94,9 @@ namespace pmo.Controllers {
             return _context.ProjectDetails
                 .IncludeAll()
                 .Include(i => i.Project)
-                    .ThenInclude(i => i.ProjectStateHistory);
+                    .ThenInclude(i => i.ProjectStateHistory)
+                .Include(i => i.Project)
+                        .ThenInclude(i => i.GateHistory);
         }
 
         private List<ProjectDetail> GetProjectDetails(ProjectState state) {
